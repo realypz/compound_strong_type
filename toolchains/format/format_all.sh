57d1131e2f3abc004358528430c9126bf567f5e5
YANG, Peizheng <yang_peizheng@outlook.com>
2024-05-22 21:52:20 +0200

    Improve the formatting report message better in `format-all.sh`

--- toolchains/format/format_all.sh
+++ toolchains/format/format_all.sh
@@ -21,8 +21,10 @@ git_diff=$(echo $(git diff))
 if [ ! -z "$git_diff" -a "$git_diff" != " " ]; then
     BOLD='\033[1m'
     NONE='\033[00m'
-    echo -e "${BOLD}Content has been modified. Please git commit the changes.${NONE}"
+    echo -e "❌ ${BOLD}The following files have been modified after formatting. Please git commit the changes.${NONE}"
+    echo $(git diff --name-only)
     exit 1
 fi
 
+echo "✅ All files are unchanged after formatting!"
 exit 0
